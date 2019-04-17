3472bd45897be89652e49eb0e8816638d667a01a
dikiardian <ardiandiki369@gmail.com>
2019-04-17 20:17:53 +0700

    add map component

---
+++ src/components/Island.vue
@@ -0,0 +1,40 @@
+<template>
+  <path @mouseover="$emit('hover')" @mouseleave="$emit('leave')" :style="isHover ? hoverStyle : {}" :fill="fill" :d="path"/>
+</template>
+
+<script>
+export default {
+  name: 'island',
+  props: {
+    fill: {
+      type: String,
+      required: true
+    },
+    isHover: {
+      type: Boolean,
+      required: true
+    },
+    province: {
+      type: String,
+      required: true
+    },
+    path: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      hoverStyle: {
+        stroke: "#002868 !important",
+        strokeWidth: "2px",
+        strokeLinejoin: "round",
+        fill: "#002868 !important",
+        cursor: "pointer"
+      }
+    };
+  }
+}
+</script>
+<style>
+</style>
---
+++ src/components/Map.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="maps">
+    <svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" width="297mm" height="210mm" version="1.1" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" image-rendering="optimizeQuality" fill-rule="evenodd" clip-rule="evenodd" viewBox="0 0 29700 21000" xmlns:xlink="http://www.w3.org/1999/xlink">
+      <Province v-for="data in masterData" :key="data.id" :fill="data.fill" :province="data.name" :paths="data.paths" />
+      <Province :fill="outlierData.fill" :province="outlierData.name" :paths="outlierData.paths" :isHoverable="false"/>
+    </svg>
+  </div>
+</template>
+
+<script>
+import Province from './Province.vue'
+import { mapState } from 'vuex'
+
+export default {
+  name: 'maps',
+  components: {
+    Province
+  },
+  data() {
+    return {
+    };
+  },
+  computed: {
+    ...mapState(['masterData', 'outlierData'])
+  }
+}
+</script>
+<style scoped>
+#map {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
